e81cd07e26f4447a5e207e9989ceaa4374e2bc47
shsu01 <shyoy707@gmail.com>
2024-06-04 17:10:01 +0900

    Add files via upload

---
+++ 9회/31114-염현서-10회0318-세로메뉴옆으로/script/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function(){
+
+    //메인의 li에 마우스를 올리면 
+    //해당 서브를 찾아서 하던거 멈추고 슬라이드다운해
+
+    $(".main>li").mouseover(function(){
+        $(this).find(".sub").stop().slideDown();
+    })
+
+    //메인의 li에서 마우스를 헤제하면
+    //이것의 서브를 찾아서 하던거 멈추고 슬라이드업해
+
+    $(".main>li").mouseout(function(){
+        $(this).find(".sub").stop().slideUp();
+    })
+
+})
