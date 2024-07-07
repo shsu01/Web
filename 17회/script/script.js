07b339caf5f98556a6ee5961d7de92c81a1252b1
shsu01 <shyoy707@gmail.com>
2024-07-07 12:34:30 +0900

    Add files via upload

---
+++ 17회/script/script.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function(){
+    $(".main li").mouseover(function(){
+        $(this).find(".sub").stop().slideDown()
+    })
+    $(".main li").mouseout(function(){
+        $(this).find(".sub").stop().slideUp()
+    })
+
+    $(".notice li:first").click(function(){
+        $(".popup").show()
+        $(".modal").show()
+    })
+
+    $("button").click(function(){
+        $(".popup").hide()
+        $(".modal").hide()
+    })
+
+    setInterval(function(){
+        $(".imglen").delay(1000)
+        $(".imglen").animate({marginLeft:-100+"%"},2000,function(){
+            $(".imglen li:first").appendTo(".imglen")
+            $(".imglen").css({marginLeft:0})
+        })
+    })
+})
